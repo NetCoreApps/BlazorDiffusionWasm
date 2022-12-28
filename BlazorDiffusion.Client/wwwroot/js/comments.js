0731023e1b87cbf55dc4e4b5c36206b14dc69a76
Demis Bellot <demis.bellot@gmail.com>
2022-12-28 18:52:44 +0800

    update static html pages

---
+++ BlazorDiffusion.Client/wwwroot/js/comments.js
@@ -0,0 +1,11 @@
+﻿var { $1 } = exports
+
+
+let ArtifactId = map($1('[data-artifact]'), x => parseInt(x.getAttribute('data-artifact')))
+
+export default {
+    template: `<div></div>`,
+    data() {
+        return { count: ArtifactId || 0 }
+    },
+}
